12d94a2eae497a968c0ed62c1135a7169a888ae9
Christine Khalil <christinekhalil@Christines-MacBook-Air.local>
2021-06-20 16:06:05 +0300

    [fix]: fixed button play function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 import Game from "./Game.js"
 import View from "./View.js"
-import Menu from "./Welcome.js"
 
 
 let view = new View(document.getElementById("game-board"))
 let game = new Game()
-let menu = new Menu()
 
 for(var i = 0; i < game.size; i++) {
     console.log("Line " + i + "th:")
@@ -15,4 +13,3 @@ for(var i = 0; i < game.size; i++) {
 }
 
 view.updateBoard(game)
-menu.update()
