102884400fdd0dad81a1df589063c46b7cdf9c48
Christine Khalil <christinekhalil@Christines-MacBook-Air.local>
2021-06-21 20:26:36 +0300

    [add]: 3D effect for cells

--- js/mainMenu.js
+++ js/mainMenu.js
@@ -3,7 +3,7 @@ const splash = document.querySelector('.splash');
 document.addEventListener("DOMContentLoaded", (e) =>{
     setTimeout(() => {
         splash.classList.add("display-none");
-    }, 4000);
+    }, 6000);
 })
 
 var startButton = document.getElementById("starts");
