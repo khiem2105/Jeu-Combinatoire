f83590e1488510d84873dbc0ce20eb203fa2ce38
Christine Khalil <christinekhalil@Christines-MacBook-Air.local>
2021-06-24 09:44:40 +0300

    [add]: print user's name on the screen

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,13 @@
 var welcomeScreen = document.getElementsByClassName("welcome")[0];
 var playButton = document.getElementsByClassName("play")[0];
-var playerName = document.getElementById("turn");
+var playerName = document.getElementById("playernameDiv");
 var nameP = document.getElementById("playername");
 var pauseButton = document.getElementById("pause");
 var mainDiv = document.getElementById("app");
 
 playButton.addEventListener("click",function(){
     pauseButton.disabled = false;
-    playerName.innerHTML = nameP.value + "'s turn";
+    playerName.innerHTML = nameP.value;
     welcomeScreen.remove();
 });
 
